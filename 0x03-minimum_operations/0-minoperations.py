772075f4ab3a7e55ed4a79ad424b9483431cecb4
jzamora5 <jzamora_5@yahoo.com>
2020-08-05 14:00:36 +0000

    Add integer validation

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -15,6 +15,9 @@ def minOperations(n):
     Returns an integer
     If n is impossible to achieve, returns 0
     """
+    if not isinstance(n, int):
+        return 0
+
     op = 0
     i = 2
     while (i <= n):
