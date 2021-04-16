d17dea3d7f8dd1afadcc0a2b93e5249daf618065
jzamora5 <jzamora_5@yahoo.com>
2021-04-15 19:45:41 -0500

    Basic files

---
+++ 0x21-substring/substring.c
@@ -0,0 +1,16 @@
+#include "substring.h"
+
+/**
+ * find_substring -  finds all the possible substrings containing a list of
+ * words, within a given string
+ * @s: string to scan
+ * @words:  array of words all substrings must be a concatenation=
+ * arrangement of
+ * @nb_words: number of elements in the array words
+ * @n: address at which to store the number of elements in the returned array
+ *
+ * Return: 1 if the pattern matches the string, or 0 if it doesn’t
+ */
+int *find_substring(char const *s, char const **words, int nb_words, int *n)
+{
+}
